ee0c31eddf41e3a8acd6a56a0617eeb446ab9a05
evaldas1994 <evaldas.tuleikis@gmail.com>
2022-11-30 17:29:04 +0200

    Fixed CategoryBalanceService

--- app/Services/analytics/CategoryBalanceService.php
+++ app/Services/analytics/CategoryBalanceService.php
@@ -15,8 +15,8 @@ class CategoryBalanceService
         foreach ($accounts as $account) {
             $transfers->add($account->transfers
                 ->where('category_id', '=', $category->id)
-                ->where('created_at', '>', Carbon::now()->firstOfMonth())
-                ->where('created_at', '<', Carbon::now()->lastOfMonth())
+                ->where('created_at', '>=', Carbon::now()->firstOfMonth())
+                ->where('created_at', '<=', Carbon::now()->lastOfMonth())
             );
         }
 
