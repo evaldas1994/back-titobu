a7b329a5dbccac78e16c27033141898a592a36a9
evaldas1994 <evaldas.tuleikis@gmail.com>
2022-11-30 21:42:37 +0200

    Run cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('command:syncCategoryBalanceToAccountBalance')->everyMinute();
+         $schedule->command('command:syncCategoryBalanceToAccountBalance')->monthly();
     }
 
     /**
